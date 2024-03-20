36b4bf89be2e42a00c7b27cdd1913cc1fdd0d636
vicky kumar mishra <mishravicky0141@gmail.com>
2024-03-21 00:58:40 +0530

    Update BookRide.js

--- Travel website/client/src/components/BookRide.js
+++ Travel website/client/src/components/BookRide.js
@@ -181,7 +181,7 @@ variables ko initialize karenge and current user jisne booking button par click 
     headers:{
       'Content-Type':'application/json',
     },
-    body:JSON.stringify({email,pass_email,passs_name,pickuptime,pickup,drivername,driverphone})
+    body:JSON.stringify({email,pass_email,pickuptime,pickup,drivername,driverphone})
     }).then(() => {
       console.log("Email sent successfully");
     })
