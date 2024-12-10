9de435ca3e2862aae10da403fbf08581748c7c9c
vicky kumar mishra <mishravicky0141@gmail.com>
2024-12-10 22:36:32 +0530

    password hide

--- Travel website/server/Connection.js
+++ Travel website/server/Connection.js
@@ -1,7 +1,7 @@
 
 
 const mongoose=require('mongoose');
-const MONGO_URI='mongodb+srv://mishravicky0141:8UezuLzi546IMzct@cluster0.th3hfpm.mongodb.net/krishna'
+const MONGO_URI='mongodb+srv://mishravicky0141:DATABASE_PASSW@cluster0.th3hfpm.mongodb.net/krishna'
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
