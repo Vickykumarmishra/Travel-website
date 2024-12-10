ed04d5383eb261d58020af6aa5e5f721dfd7303f
vicky kumar mishra <mishravicky0141@gmail.com>
2024-12-10 22:57:12 +0530

    password env update

--- Travel website/server/Connection.js
+++ Travel website/server/Connection.js
@@ -1,11 +1,9 @@
 
-
+require('dotenv').config();
 const mongoose=require('mongoose');
-const MONGO_URI='mongodb+srv://mishravicky0141:DATABASE_PASSW@cluster0.th3hfpm.mongodb.net/krishna'
+const MONGO_URI='mongodb+srv://mishravicky0141:${process.env.DATABASE_PASSW}@cluster0.th3hfpm.mongodb.net/krishna'
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-
-  // fRlM5OHjJqmLKvsz
